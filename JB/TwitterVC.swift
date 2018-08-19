e4d41a80418fd1c4caf6ccadf75e8cea65296bd8
Raymond Li <raymond.s.li@gmail.com>
2018-08-19 14:22:44 -0700

    Fix some formatting

--- JB/TwitterVC.swift
+++ JB/TwitterVC.swift
@@ -11,12 +11,21 @@ import UIKit
 class TwitterVC: UIViewController {
     
     var twitterHandle = "NBA"
+    @IBOutlet weak var twitterButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        twitterButton.setTitle("@" + twitterHandle,for: .normal)
         
         let childTwitter = self.childViewControllers.last as! JBTwitterTimeline
         childTwitter.twitterHandle = twitterHandle
+    }
+    
+    @IBAction func twitterPressed(_ sender: Any) {
+        let childTwitter = self.childViewControllers.last as! JBTwitterTimeline
+        if childTwitter.tableView.numberOfRows(inSection: 0) > 0 {
+            childTwitter.tableView.scrollToRow(at: IndexPath(row: 0, section: 0), at: .top, animated: true)
+        }
     }
     
     @IBAction func backPressed(_ sender: Any) {
